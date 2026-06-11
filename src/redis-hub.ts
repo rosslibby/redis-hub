560627716998955354b7f9e300555f0fdd4eaaf9
Ross Libby <rosslibby92@gmail.com>
2026-06-11 17:35:11 -0400

    delete client from hub state upon connection termination & assign unique ID to each RedisHub instance

--- src/redis-hub.ts
+++ src/redis-hub.ts
@@ -1,3 +1,4 @@
+import crypto from 'crypto';
 import { createClient, RedisClientOptions } from 'redis';
 import { RedisClient, LoggerConfig, LogResult, ClientOptions } from './types';
 import { Logger } from './utils';
@@ -12,6 +13,7 @@ const logger = new Logger();
  * are just distinct names.
  */
 export class RedisHub {
+  public hubId: string = crypto.randomUUID();
   private defaultClientName: string = 'default';
   private clients: Record<string, RedisClient> = {};
   private clientOptions: Record<string, RedisClientOptions> = {};
@@ -150,7 +152,7 @@ export class RedisHub {
     client.on('end', () => {
       this.status = `[${clientId}]: client closed.`;
       logger.log(this.status);
-      this.clients[clientId] = client;
+      delete this.clients[clientId];
     });
     client.on('error', (err) => {
       this.status = `[${clientId}]: client error:`
